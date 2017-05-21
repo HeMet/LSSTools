fc4fb687a9ba7ff88cb35b98d4a781f820eab15e
HeMet <hemet.mail@gmail.com>
2017-05-21 11:41:55 +0400

    Updates Antlr4 runtime.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "LSS",
     dependencies: [
-        .Package(url: "https://github.com/HeMet/ANTLR4Swift.git", "0.1.2")
+        .Package(url: "https://github.com/HeMet/ANTLR4Swift.git", "0.2.0")
     ],
     exclude: ["Sources/ifconvert/Grammar", "Sources/ifconvert/Samples"]
 )
